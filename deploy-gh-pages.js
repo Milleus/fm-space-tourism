a7c3da2d27eb2aa571e2a84abf9fe14465711c93
Dave Quah <quahdave@gmail.com>
2022-03-09 13:28:32 +0800

    chore: add deployment script

---
+++ deploy-gh-pages.js
@@ -0,0 +1,14 @@
+import ghpages from "gh-pages";
+
+ghpages.publish(
+  "public",
+  {
+    branch: "gh-pages",
+    message: "deploy: update website"
+  },
+  () => {
+    console.log(
+      "Deployment completed: https://milleus.github.io/portfolio-space-tourism/"
+    );
+  }
+);
